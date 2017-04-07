6990b8a3759a22adc9ed82ec90f6cae80b089b6c
yusuke_ogiwara <yusuke_ogiwara@dwango.co.jp>
2017-04-07 12:23:36 +0900

    Add DynamicFont spec

--- spec/DynamicFontSpec.js
+++ spec/DynamicFontSpec.js
@@ -49,6 +49,35 @@ describe("test DynamicFont", function() {
 			game: runtime.game,
 			fontFamily: g.FontFamily.SansSerif,
 			size: 20,
+			hint: {},
+			fontColor: "white",
+			fontWeight: g.FontWeight.Bold,
+			strokeWidth: 1,
+			strokeColor: "red",
+			strokeOnly: true
+		});
+		expect(font.fontFamily).toBe(g.FontFamily.SansSerif);
+		expect(font.size).toBe(20);
+		expect(font.fontColor).toBe("white");
+		expect(font.fontWeight).toBe(g.FontWeight.Bold);
+		expect(font.strokeWidth).toBe(1);
+		expect(font.strokeColor).toBe("red");
+		expect(font.strokeOnly).toBe(true);
+		expect(font.hint).toEqual({
+				initialAtlasWidth: 2048,
+				initialAtlasHeight: 2048,
+				maxAtlasWidth: 2048,
+				maxAtlasHeight: 2048,
+				maxAtlasNum: 1
+		});
+	});
+	it("初期化 - Given hint", function() {
+		var runtime = skeletonRuntime();
+
+		var font = new g.DynamicFont({
+			game: runtime.game,
+			fontFamily: g.FontFamily.SansSerif,
+			size: 20,
 			hint: {
 				initialAtlasWidth: 1000,
 				initialAtlasHeight: 2000,
