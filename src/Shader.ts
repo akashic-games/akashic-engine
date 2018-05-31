2641f76c70e3509f8961ba0e8a22b6a7b0ce34ea
yusuke_ogiwara <yusuke_ogiwara@dwango.co.jp>
2018-05-31 14:10:13 +0900

    Add comment

--- src/Shader.ts
+++ src/Shader.ts
@@ -82,6 +82,9 @@ namespace g {
 		/**
 		 * uniform値。
 		 * この値の型は `ShaderProgram` の生成時にのみ指定可能であり、変更してはならない。
+		 *
+		 * 例えば `type` に `"float"` を指定して `value` に `[0.0, 1.0]` のような配列型を指定した場合、
+		 * それ以降 `value` に `0.0` のような数値を代入することはできない。
 		 */
 		value: number | Int32Array | Float32Array;
 	}
