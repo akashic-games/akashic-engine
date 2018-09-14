9879530b074220b2aa01a9aa2bd64abf77fd965a
xnv <xnv@users.noreply.github.com>
2018-09-14 14:29:02 +0900

    Revert "cherry-pick #87 (add-skipping-changed-ae2x, a48266d)"

--- spec/GameSpec.js
+++ spec/GameSpec.js
@@ -25,7 +25,6 @@ describe("test Game", function() {
 		expect(game.height).toBe(270);
 		expect(game.selfId).toBe("foo");
 		expect(game.playId).toBe(undefined);
-		expect(game.skippingChanged).not.toBe(undefined);
 		expect(game).toHaveProperty("_assetManager");
 		expect(game).toHaveProperty("_initialScene");
 	});
@@ -42,7 +41,6 @@ describe("test Game", function() {
 		expect(game.external).toEqual({});  // external は触らない
 		expect(game.vars).toEqual({});  // vars も触らない
 		expect(game.playId).toBe(undefined);
-		expect(game.skippingChanged).toBe(undefined);
 	});
 
 	it("global assets", function (done) {
