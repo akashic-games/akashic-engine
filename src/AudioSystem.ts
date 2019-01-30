1c5d63470b258fde68dbc1bd7a467ca38d6501d8
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2019-01-30 12:57:53 +0900

    Update comment

--- src/AudioSystem.ts
+++ src/AudioSystem.ts
@@ -320,7 +320,7 @@ namespace g {
 			if (e.player._supportsPlaybackRate())
 				return;
 
-			// 再生速度非対応の場合のフォールバック: 鳴らさず即止める
+			// 再生速度非対応の場合のフォールバック: 鳴らさず即ミュートにする
 			if (this._playbackRate !== 1.0) {
 				e.player.stop();
 			}
