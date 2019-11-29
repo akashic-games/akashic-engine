7c41fa5054b0d9bee7c805df2a7bfb0a878fa201
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2019-11-29 13:10:43 +0900

    Support a legacy specification of default anchor

--- src/__tests__/MatrixSpec.ts
+++ src/__tests__/MatrixSpec.ts
@@ -63,4 +63,48 @@ describe("test Matrix", () => {
 
 		expect(m._matrix).toBeNear(expected._matrix, 10);
 	});
+
+	it("deprecated: updateWithoutAnchor", () => {
+		const m = new PlainMatrix();
+		const angle = 50;
+		const rad = (angle * Math.PI) / 180;
+		const cosValue = Math.cos(rad);
+		const sinValue = Math.sin(rad);
+		m.updateWithoutAnchor(10, 8, 2, 3, angle, 100, 50);
+		const expected = new PlainMatrix();
+		const tmp = new PlainMatrix();
+		tmp._matrix = [1, 0, 0, 1, 10 / 2, 8 / 2];
+		expected.multiply(tmp);
+		tmp._matrix = [1, 0, 0, 1, 100, 50];
+		expected.multiply(tmp);
+		tmp._matrix = [cosValue, sinValue, -sinValue, cosValue, 0, 0];
+		expected.multiply(tmp);
+		tmp._matrix = [2, 0, 0, 3, 0, 0];
+		expected.multiply(tmp);
+		tmp._matrix = [1, 0, 0, 1, -10 / 2, -8 / 2];
+		expected.multiply(tmp);
+		expect(m._matrix).toBeNear(expected._matrix, 10);
+	});
+
+	it("deprecated: updateByInverseWithoutAnchor", () => {
+		const m = new PlainMatrix();
+		const angle = 50;
+		const rad = (angle * Math.PI) / 180;
+		const cosValue = Math.cos(rad);
+		const sinValue = Math.sin(rad);
+		m.updateByInverseWithoutAnchor(10, 8, 2, 3, angle, 100, 50);
+		const expected = new PlainMatrix();
+		const tmp = new PlainMatrix();
+		tmp._matrix = [1, 0, 0, 1, 10 / 2, 8 / 2];
+		expected.multiply(tmp);
+		tmp._matrix = [1 / 2, 0, 0, 1 / 3, 0, 0];
+		expected.multiply(tmp);
+		tmp._matrix = [cosValue, -sinValue, sinValue, cosValue, 0, 0];
+		expected.multiply(tmp);
+		tmp._matrix = [1, 0, 0, 1, -100, -50];
+		expected.multiply(tmp);
+		tmp._matrix = [1, 0, 0, 1, -10 / 2, -8 / 2];
+		expected.multiply(tmp);
+		expect(m._matrix).toBeNear(expected._matrix, 10);
+	});
 });
