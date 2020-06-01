64a84cd72c381d3ff93605ee586d6c2376b0d6ae
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2020-06-01 15:08:29 +0900

    remove pdi-common-impls AudioSystem, VideoSystem

--- src/pdi-common-impls/VideoSystem.ts
+++
@@ -1,9 +0,0 @@
-import { VideoSystemLike } from "../pdi-types/VideoSystemLike";
-
-/**
- * 将来 VideoPlayerインスタンスの一元管理（ボリューム設定などAudioSystemと似た役割）
- * を担うインターフェース。VideoAssetはVideoSystemを持つという体裁を整えるために(中身が空であるが)
- * 定義されている。
- * TODO: 実装
- */
-export class VideoSystem implements VideoSystemLike {}
