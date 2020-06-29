d05a3f44eea560a50ac92b50bd288e37a5e65571
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2020-06-29 17:09:20 +0900

    remove getGameInAssetContext()

--- src/AssetManager.ts
+++ src/AssetManager.ts
@@ -16,7 +16,6 @@ import { AssetManagerLoadHandler } from "./AssetManagerLoadHandler";
 import { AudioSystemManager } from "./AudioSystemManager";
 import { DynamicAssetConfiguration } from "./DynamicAssetConfiguration";
 import { ExceptionFactory } from "./ExceptionFactory";
-import { getGameInAssetContext } from "./getGameInAssetContext";
 import { VideoSystem } from "./VideoSystem";
 
 export type OneOfAsset = AudioAsset | ImageAsset | ScriptAsset | TextAsset | VideoAsset;
@@ -186,18 +185,17 @@ export class AssetManager implements AssetLoadHandler {
 	/**
 	 * `AssetManager` のインスタンスを生成する。
 	 *
-	 * @param gameParams このインスタンスが属するゲーム。ゲーム開発者が指定する必要はない。
+	 * @param gameParams このインスタンスが属するゲーム。
 	 * @param conf このアセットマネージャに与えるアセット定義。game.json の `"assets"` に相当。
 	 * @param audioSystemConfMap このアセットマネージャに与えるオーディオシステムの宣言。
 	 * @param moduleMainScripts このアセットマネージャに与える require() 解決用のエントリポイント。
 	 */
 	constructor(
-		gameParams?: AssetManagerParameterGameLike,
+		gameParams: AssetManagerParameterGameLike,
 		conf?: AssetConfigurationMap,
 		audioSystemConfMap?: AudioSystemConfigurationMap,
 		moduleMainScripts?: ModuleMainScriptsMap
 	) {
-		gameParams = gameParams || getGameInAssetContext();
 		this._resourceFactory = gameParams.resourceFactory;
 		this._audioSystemManager = gameParams.audio;
 		this._defaultAudioSystemId = gameParams.defaultAudioSystemId;
--- src/DefaultLoadingScene.ts
+++ src/DefaultLoadingScene.ts
@@ -15,10 +15,9 @@ import { Scene } from "./Scene";
 export interface DefaultLoadingSceneParameterObject {
 	/**
 	 * このシーンが属する `Game` 。
-	 * ゲーム開発者が指定する必要はない。
 	 * @default g.game
 	 */
-	game?: Game;
+	game: Game;
 	style?: "default" | "compact";
 }
 
@@ -89,7 +88,7 @@ export class DefaultLoadingScene extends LoadingScene {
 	 * `DeafultLoadingScene` のインスタンスを生成する。
 	 * @param param 初期化に用いるパラメータのオブジェクト
 	 */
-	constructor(param: DefaultLoadingSceneParameterObject = {}) {
+	constructor(param: DefaultLoadingSceneParameterObject) {
 		super({ game: param.game, name: "akashic:default-loading-scene" });
 		if (param.style === "compact") {
 			this._barWidth = this.game.width / 4;
