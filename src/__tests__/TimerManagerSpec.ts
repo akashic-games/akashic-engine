33767bc4497b23750e1f30cebbdbc651d69c7a21
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2020-08-31 11:54:31 +0900

    Modify CHANGELOG, TimerManagerSpec

--- src/__tests__/TimerManagerSpec.ts
+++ src/__tests__/TimerManagerSpec.ts
@@ -559,15 +559,14 @@ describe("test TimerManager", () => {
 		expect(m._identifiers).toBeUndefined();
 	});
 
-	it("TimerManager#setInterval() does not run after clearInterval()", () => {
+	it("clearInterval - same time", () => {
 		const m = new TimerManager(trigger, 10);
 		let cnt1 = 0;
 		let cnt2 = 0;
 		let timer2: TimerIdentifier;
 
 		const timer1 = m.setInterval(() => {
-			// 同タイミングでタイマが完了した場合、先行するタイマの処理でもうひとつのタイマを clearInterval() するとクリアしたのにハンドラを実行してエラーとなっていた。
-			// clearInterval() 後に timer2 のハンドラが実行されない事を確認する。
+			// 同タイミングでタイマが完了した場合、先行する timer1 の処理で timer2 を clearInterval() しても timer2 のハンドラが実行されない事を確認する。
 			if (!timer2.destroyed()) m.clearInterval(timer2);
 			cnt1++;
 		}, 200);
