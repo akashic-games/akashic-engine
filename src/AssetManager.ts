a887302cbe8c25f816377ffde4c3d77567c62751
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2020-12-17 09:00:38 +0900

    chore: return early if accessorPath started with `*`

--- src/AssetManager.ts
+++ src/AssetManager.ts
@@ -629,7 +629,12 @@ export class AssetManager implements AssetLoadHandler {
 	 * @private
 	 */
 	_replaceModulePathToAbsolute(accessorPath: string): string {
-		if (accessorPath[0] === "/") return accessorPath;
+		if (
+			accessorPath[0] === "/" ||
+			accessorPath[0] === "*" // パスに `**/*` が指定された場合
+		) {
+			return accessorPath;
+		}
 
 		for (const moduleName in this._moduleMainScripts) {
 			if (!this._moduleMainScripts.hasOwnProperty(moduleName)) continue;
