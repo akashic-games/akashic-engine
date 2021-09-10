bf270180a98e31d1bb6728f2da34c4d9878edff0
yu-ogi <yusuke_ogiwara@dwango.co.jp>
2021-09-10 18:19:39 +0900

    fix: remove redundant code

--- src/__tests__/helpers/mock.ts
+++ src/__tests__/helpers/mock.ts
@@ -437,52 +437,20 @@ export class VideoAsset extends pci.VideoAsset {
 }
 
 export class VectorImageAsset extends pci.VectorImageAsset {
-	id: string;
-	path: string;
-	originalPath: string;
-	type: "vector-image" = "vector-image";
-	width: number;
-	height: number;
-	hint: g.VectorImageAssetHint | undefined;
 
-	onDestroyed: g.Trigger<g.Asset> = new g.Trigger();
-
-	constructor(id: string, path: string, width: number, height: number, hint?: g.VectorImageAssetHint) {
-		super(id, path, width, height, hint);
-		this.id = id;
-		this.path = path;
-		this.originalPath = path;
-		this.width = width;
-		this.height = height;
-		this.hint = hint;
-	}
-
-	createSurface(_width: number, _height: number, _sx?: number, _sy?: number, _sWidth?: number, _sHeight?: number): Surface | null {
+	createSurface(
+		_width: number,
+		_height: number,
+		_sx?: number,
+		_sy?: number,
+		_sWidth?: number,
+		_sHeight?: number
+	): Surface | null {
 		return null;
-	}
-
-	inUse(): boolean {
-		return false;
-	}
-
-	destroy(): void {
-		if (this.destroyed()) {
-			return;
-		}
-		this.onDestroyed.destroy();
-		this.onDestroyed = undefined!;
-	}
-
-	destroyed(): boolean {
-		return !this.onDestroyed;
 	}
 
 	_load(loader: AssetLoadHandler): void {
 		loader._onAssetLoad(this);
-	}
-
-	_assetPathFilter(path: string): string {
-		return path;
 	}
 }
 
